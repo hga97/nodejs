0446816ba915d07c9f5777e94fe539a8166a6843
hga <841428324@qq.com>
2020-07-15 00:00:37 +0800

    4.9完结

--- models/posts.js
+++ models/posts.js
@@ -1,31 +1,78 @@
 const marked = require("marked")
 const Post = require("../lib/mongo").Post
 
-Post.plugin("contentToHtml",{
-    afterFind:function(posts){
-        return posts.map(function(post){
-            post.content = marked(post.content)
-            return post
-        })
-    },
-    afterFindOne: function(post){
-        if(post){
-            post.content = marked(post.content)
-        }
-        return post
+Post.plugin('contentToHtml', {
+  afterFind: function (posts) {
+    return posts.map(function (post) {
+      post.content = marked(post.content)
+      return post
+    })
+  },
+  afterFindOne: function (post) {
+    if (post) {
+      post.content = marked(post.content)
     }
+    return post
+  }
 })
 
 module.exports = {
-    create: function create(post){
-        return Post.create(post).exec()
-    },
-    getPostById:function getPostById(postId){
-        return Post
-        .findOne({_id: postId})
-        .populate({path:'author',model:'User'})
-        .addCreatedAt()
-        .contentToHtml()
-        .exec()
+  create: function create(post) {
+    return Post.create(post).exec()
+  },
+  getPostById: function getPostById(postId) {
+    return Post
+      .findOne({
+        _id: postId
+      })
+      .populate({
+        path: 'author',
+        model: 'User'
+      })
+      .addCreatedAt()
+      .contentToHtml()
+      .exec()
+  },
+  getPosts: function getPosts(author) {
+    const query = {}
+    if (author) {
+      query.author = author
     }
+
+    return Post
+      .find(query)
+      .populate({
+        path: 'author',
+        model: 'User'
+      })
+      .sort({
+        _id: -1
+      })
+      .addCreatedAt()
+      .contentToHtml()
+      .exec()
+  },
+  incPv: function incPv(postId) {
+    return Post
+      .update({
+        _id: postId
+      }, {
+        $inc: {
+          pv: 1
+        }
+      })
+      .exec()
+  },
+  getRawPostById: function getRawPostById(postId) {
+    return Post
+      .findOne({_id:postId})
+      .populate({path:'author',model:'User'})
+      .exec()
+  },
+  updatePostById:function updatePostById(postId,data){
+    return Post.update({_id:postId},{$set:data}).exec()
+  },
+  delPostById: function delPostById(postId){
+    return Post.deleteOne({_id:postId}).exec()
+  }
 }
